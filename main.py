55495661a9cfc5858dc0c542b6cd5a0e9ffaa7e1
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-09-06 14:33:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+#import module1
+
+#computer will choose a random number between 1 - 10
+random_number = random.randint(1, 10)
+
+print(random_number)
+#print(module1.pi)
+
+#creates a random number between 0 and 5
+random_float = random.random() * 5
+
+print(random_float)
